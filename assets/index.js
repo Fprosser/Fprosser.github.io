226f8603365c9b2951e0000a1297b00b06803316
Fin <finton.prosser@scscloud.com>
2023-06-06 23:06:33 -0400

    develop project load

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+
+const main = () => {
+    loadProjects();
+    console.log("Hello, World");
+}
+
+async function loadProjects() {
+    const response = await fetch(".assets\projects.json");
+    const jsonData = await response.json();
+    console.log(jsonData);
+    
+}
